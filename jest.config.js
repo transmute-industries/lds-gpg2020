7f9267e12e254b11336684a5dd2004903d3f1724
Orie Steele <orie@transmute.industries>
2020-02-16 15:51:08 -0600

    chore: ignore bin coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   verbose: true,
   testPathIgnorePatterns: ["__fixtures__"],
-  coveragePathIgnorePatterns: ["__fixtures__"]
+  coveragePathIgnorePatterns: ["__fixtures__", "bin"]
 };
